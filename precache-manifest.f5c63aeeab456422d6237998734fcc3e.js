210e5b9e8a7692e361fdfe93a509e34f74db2579
Marko Herceg <hyperstassive@gmail.com>
2020-03-14 21:14:52 +0100

    Updates

---
+++ precache-manifest.f5c63aeeab456422d6237998734fcc3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac035061c2d27f8a63a72112fbc90233",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "670677a5c1ac5dbedf01",
+    "url": "/monsters-rolodex/static/css/main.57170787.chunk.css"
+  },
+  {
+    "revision": "c7c060b7febb674bc7a3",
+    "url": "/monsters-rolodex/static/js/2.78892bf9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.78892bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "670677a5c1ac5dbedf01",
+    "url": "/monsters-rolodex/static/js/main.519ebce5.chunk.js"
+  },
+  {
+    "revision": "24034436bd08b53a5646",
+    "url": "/monsters-rolodex/static/js/runtime-main.6386504b.js"
+  }
+]);
