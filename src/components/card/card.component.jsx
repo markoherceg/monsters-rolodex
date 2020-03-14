0e26a47eee95acc1a5036c65985dcafa50408892
Marko Herceg <hyperstassive@gmail.com>
2020-03-14 21:16:30 +0100

    Published to GitHub pages

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 
-import "./card.styles.css";
+import './card.styles.css';
 
 export const Card = props => (
   <div className="card-container">
@@ -8,7 +8,7 @@ export const Card = props => (
       alt="monster"
       src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
     />
-    <h1>{props.monster.name}</h1>
+    <h2>{props.monster.name}</h2>
     <p> {props.monster.email}</p>
   </div>
 );
